b157815677188006a31129f97a524b93dd757534
Łukasz Augustyniak <luk.augustyniak@gmail.com>
2016-04-12 16:57:57 +0200

    errors update based on gensim (0.12.4), shuffle and docsvecs

--- word2vec-sentiments.py
+++ word2vec-sentiments.py
@@ -1,8 +1,10 @@
 # gensim modules
 from gensim import utils
-# from gensim.models.doc2vec import LabeledSentence
 from gensim.models.doc2vec import TaggedDocument
 from gensim.models import Doc2Vec
+
+# random shuffle
+from random import shuffle
 
 # numpy
 import numpy
@@ -50,7 +52,8 @@ class TaggedLineSentence(object):
         return self.sentences
 
     def sentences_perm(self):
-        # return numpy.random.permutation(self.sentences)
+        shuffle(self.sentences)
+	return self.sentences
         
 
 log.info('source load')
@@ -79,8 +82,8 @@ train_labels = numpy.zeros(25000)
 for i in range(12500):
     prefix_train_pos = 'TRAIN_POS_' + str(i)
     prefix_train_neg = 'TRAIN_NEG_' + str(i)
-    train_arrays[i] = model[prefix_train_pos]
-    train_arrays[12500 + i] = model[prefix_train_neg]
+    train_arrays[i] = model.docvecs[prefix_train_pos]
+    train_arrays[12500 + i] = model.docvecs[prefix_train_neg]
     train_labels[i] = 1
     train_labels[12500 + i] = 0
 
@@ -92,8 +95,8 @@ test_labels = numpy.zeros(25000)
 for i in range(12500):
     prefix_test_pos = 'TEST_POS_' + str(i)
     prefix_test_neg = 'TEST_NEG_' + str(i)
-    test_arrays[i] = model[prefix_test_pos]
-    test_arrays[12500 + i] = model[prefix_test_neg]
+    test_arrays[i] = model.docvecs[prefix_test_pos]
+    test_arrays[12500 + i] = model.docvecs[prefix_test_neg]
     test_labels[i] = 1
     test_labels[12500 + i] = 0
 
